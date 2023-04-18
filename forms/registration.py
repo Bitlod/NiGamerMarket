52b0e01582047fdf40a0c880545c4effa903e41b
4e6yPeK48 <71962981+4e6yPeK48@users.noreply.github.com>
2023-04-18 09:38:09 +0300

    Update registration.py
    коммент

--- forms/registration.py
+++ forms/registration.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm):  # форма регистрации 
     username = StringField('Имя пользователя', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
