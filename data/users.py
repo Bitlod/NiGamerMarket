ad732a3507a2b67ab4c6cfe80cdac63d622dd22b
4e6yPeK48 <71962981+4e6yPeK48@users.noreply.github.com>
2023-04-18 09:36:32 +0300

    Update users.py
    комменты

--- data/users.py
+++ data/users.py
@@ -1,22 +1,22 @@
 # primary_key, foreign_key, autoincrement
 
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, orm
-import datetime
+import datetime  # можно удалить
 from sqlalchemy_serializer import SerializerMixin
 from data.db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-class User(SqlAlchemyBase, SerializerMixin, UserMixin):
+class User(SqlAlchemyBase, SerializerMixin, UserMixin):  # класс пользователя, юзер миксин для сессии
     __tablename__ = 'users'
     id = Column(Integer, autoincrement=True, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
     hashed_password = Column(String)
 
-    def set_password(self, password):
+    def set_password(self, password):  # хеш пароля
         self.hashed_password = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password):  # проверка хеша пароля
         return check_password_hash(self.hashed_password, password)
