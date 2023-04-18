ec81e75957d07aaeaa799c0bde17018820221f54
4e6yPeK48 <71962981+4e6yPeK48@users.noreply.github.com>
2023-04-18 09:37:38 +0300

    Update login.py
    коммент

--- forms/login.py
+++ forms/login.py
@@ -3,7 +3,7 @@ from wtforms.fields import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm):  # форма логина
     username = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
