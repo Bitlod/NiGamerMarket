26f7db507a9bb6bc654b15b5c3ea94f89abecaf0
4e6yPeK48 <71962981+4e6yPeK48@users.noreply.github.com>
2023-04-18 09:33:40 +0300

    Update products.py
    коммент

--- data/products.py
+++ data/products.py
@@ -4,14 +4,14 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, o
 from data import db_session
 
 
-class Product(SqlAlchemyBase, SerializerMixin):
+class Product(SqlAlchemyBase, SerializerMixin):  # класс товаров
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)
     name = Column(String(100), nullable=False)
     price = Column(Integer, nullable=False)
 
-    def delete(self, product_id):
+    def delete(self, product_id):  # удаление товара 
         db_sess = db_session.create_session()
         product = db_sess.query(Product).get(product_id)
         if product:
